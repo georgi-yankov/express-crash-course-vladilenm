0dfaabbbb1831202059aedf5c9d2559c405d9fe7
Georgi Yankov <georgi.st.yankov@gmail.com>
2022-02-16 15:27:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import path from "path";
+import { requestTime, logger } from "./middlewares.js";
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs"));
+
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(requestTime);
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Main Page", active: "main" });
+});
+
+app.get("/features", (req, res) => {
+  res.render("features", { title: "Features Page", active: "features" });
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "static/index.html"));
+// });
+
+// app.get("/download", (req, res) => {
+//   res.download(path.resolve(__dirname, "static/index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}...`);
+});
